870ceb159cfef23c2fa674be0a84f662be3f05eb
g-bolsoni <giovanebolsoni2@gmail.com>
2023-11-22 21:53:30 -0300

    Add clients for room

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,7 +1,8 @@
-import { log } from "console";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
 import { useNavigate  } from "react-router-dom";
+import { v4 as uuidV4} from "uuid"
+import Peer from "peerjs";
 
 const WS = "http://localhost:8080";
 
@@ -14,15 +15,34 @@ interface RoomProviderProps {
 
 export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({ children }) => {
     const navigate = useNavigate();
+    const [ me, setMe ] = useState<Peer>(); //represent our current peer
+
+
     const enterRoom = ( { roomId }: { roomId: "string"}) => {
         console.log({ roomId });
         navigate(`/room/${roomId}`)
     }
+
+
+    const getUsers = ({ participants }: { participants: string[] }) => {
+        console.log(participants);
+
+    }
+
+
     useEffect(() => {
+        const meId = uuidV4();
+        const peer =  new Peer(meId);
+        setMe(peer);
+
         ws.on('room-created', enterRoom)
+        ws.on('get-users', getUsers)
+
+
     }, []);
+
     return(
-        <RoomContext.Provider value={{ ws }}>
+        <RoomContext.Provider value={{ ws, me }}>
             { children }
         </RoomContext.Provider>
     )
